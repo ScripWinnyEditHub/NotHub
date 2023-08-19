26d15e2532e77e74b7f66548aeca6f3a5fe68feb
ScripWinnyEditHub <142580108+ScripWinnyEditHub@users.noreply.github.com>
2023-08-19 22:10:44 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -316,18 +316,6 @@ if (_G.Team == "Pirates" or _G.Team == "Marines") and not join then
 
                 end
 
-            elseif _G.Team == "Marines" the
-
-                for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
-
-                    for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton[v])) do
-
-                        v.Function()
-
-                    end
-
-                end
-
             else
 
                 for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
